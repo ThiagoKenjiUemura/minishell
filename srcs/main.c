793251e291f6df5ae66e1c5e8b8e42c174ba7e5e
Thiago Kenji Uemura <tkenji-u@c2r7p3.42sp.org.br>
2025-10-28 19:24:40 -0300

    parsing inputs

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 15:59:13 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/10/28 17:43:56 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/10/28 19:14:50 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@ static void	minishell_loop(t_shell *data)
 	{
 		data->input = readline("minishel> ");
 		if (!data->input)
+		{
+			ft_printf("exit\n");
 			break ;
+		}
+		if (data->input[0])
+			add_history(data->input);
+		free(data->input);
 	}
 }
 int	main(int argc, char **argv, char **envp)
@@ -28,6 +34,5 @@ int	main(int argc, char **argv, char **envp)
 	data = ft_calloc(1, sizeof(t_shell));
 	(void)argv;
 	(void)argc;
-	(void)envp;
 	minishell_loop(data);
 }
--- srcs/parsing/input_parser.c
+++ srcs/parsing/input_parser.c
@@ -6,9 +6,15 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:56:16 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/10/28 17:13:32 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/10/28 19:24:25 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+bool	parse_just_spaces(char *input)
+{
+	int	i;
+	
+	i = 0;
+}
