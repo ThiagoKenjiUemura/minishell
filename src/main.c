6dd630388948ed9ae160b95dee65f97ab1326424
Livia Ferreira Dos Santos <liferrei@c3r6p5.42sp.org.br>
2025-11-05 17:40:49 -0300

    Livia

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 15:59:13 by tkenji-u          #+#    #+#             */
-/*   Updated: 2025/11/05 17:30:41 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/05 17:40:26 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,12 @@ static void	minishell_loop(t_shell *data)
 		}
 		if (*data->input)
 			add_history(data->input);
-		free(data->input);
-		data->input = NULL; 
+		if (garbage_add(data, data->input))
+		{
+			free(data->input);
+			break ;
+		}
+		data->input = NULL;
 	}
 }
 
@@ -38,21 +42,25 @@ static void	init_signals(void)
 int	main(int argc, char **argv, char **envp)
 {
 	t_shell	*data;
-	
+
 	(void)argv;
-	if(!check_argc(argc))
+	if (!check_argc(argc))
 		return (1);
 	init_signals();
+
 	data = ft_calloc(1, sizeof(t_shell));
 	if (!data)
 		return (1);
-	data->garbage = NULL; 
-	data->envp = init_envp(data, envp); 
+	data->garbage = NULL;
+
+	data->envp = init_envp(data, envp);
 	if (!data->envp)
 	{
+		garbage_free_all(data);
 		free(data);
 		return (1);
 	}
+
 	minishell_loop(data);
 	free_shell(data); 
 	return (0);
