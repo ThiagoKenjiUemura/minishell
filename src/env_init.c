5339dd8eda4d31b5fee10d9b1454e51feb18f335
Livia Ferreira Dos Santos <liferrei@c3r6p5.42sp.org.br>
2025-11-05 17:43:56 -0300

    Livia

--- src/env_init.c
+++ src/env_init.c
@@ -6,49 +6,17 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 11:43:33 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/05 17:29:04 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/05 17:42:43 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	free_envp(char **envp)
-{
-	int	i;
-
-	if (!envp)
-		return ;
-	i = 0;
-	while (envp[i])
-	{
-		free(envp[i]);
-		i++;
-	}
-	free(envp);
-}
-
-static void	free_env_copy(char **copy, int size)
-{
-	int	i;
-
-	i = 0;
-	while (i < size)
-	{
-		free(copy[i]);
-		i++;
-	}
-	free(copy);
-}
 
 void	free_shell(t_shell *data)
 {
 	if (!data)
 		return ;
 	gc_free_all(data);
-	if (data->envp)
-		free_envp(data->envp);
-	if (data->input)
-		free(data->input);
 	free(data);
 }
 
