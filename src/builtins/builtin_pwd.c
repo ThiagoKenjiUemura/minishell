94afedb26394e539e329f8d9572328794536b1eb
Livia Ferreira Dos Santos <liferrei@c1r6p4.42sp.org.br>
2025-11-17 16:43:14 -0300

    feat: add builtin pwd

--- src/builtins/builtin_pwd.c
+++ src/builtins/builtin_pwd.c
@@ -6,9 +6,20 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 12:15:41 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/17 16:37:41 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/17 16:41:11 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int ft_pwd(void)
+{
+	char buf[4096];
+	if (getcwd(buf, sizeof(buf)))
+	{
+		ft_printf("%s\n", buf);
+		return (0);
+	}
+	perror("pwd");
+	return (1);
+}
