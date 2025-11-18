cfadd84b8a995b117ee099aeb0b207ff496f3ed8
Livia Ferreira Dos Santos <liferrei@c1r4p2.42sp.org.br>
2025-11-18 10:10:36 -0300

    style: buitin_exit

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,16 +6,15 @@
 /*   By: liferrei <liferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/05 12:30:07 by liferrei          #+#    #+#             */
-/*   Updated: 2025/11/17 20:45:06 by liferrei         ###   ########.fr       */
+/*   Updated: 2025/11/18 10:09:11 by liferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int ft_exit(t_shell *shell, t_cmd *cmd)
+int	ft_exit(t_shell *shell, t_cmd *cmd)
 {
 	ft_printf("exit\n");
-	
 	if (!cmd->args[1])
 	{
 		shell->running = 0;
@@ -24,10 +23,9 @@ int ft_exit(t_shell *shell, t_cmd *cmd)
 	if (cmd->args[1][0] == '\0' || !ft_str_is_numeric(cmd->args[1]))
 	{
 		if (cmd->args[1][0] == '\0')
-            ft_printf("exit: : numeric argument required\n");
-        else
-            ft_printf("exit: %s: numeric argument required\n",
-                cmd->args[1]);
+			ft_printf("exit: : numeric argument required\n");
+		else
+			ft_printf("exit: %s: numeric argument required\n", cmd->args[1]);
 		shell->running = 0;
 		shell->last_exit_status = 2;
 		return (2);
@@ -42,4 +40,3 @@ int ft_exit(t_shell *shell, t_cmd *cmd)
 	shell->running = 0;
 	return (shell->last_exit_status);
 }
-
