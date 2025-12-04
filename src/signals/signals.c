1ce79cc056b8c1ee70a61699848fae00538bf2f2
Thiago Kenji Uemura <tkenji-u@c3r4p2.42sp.org.br>
2025-12-04 11:47:21 -0300

    Fixing norm errors

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -6,7 +6,7 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 19:31:07 by thiagouemur       #+#    #+#             */
-/*   Updated: 2025/12/04 11:37:03 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/12/04 11:46:47 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,19 @@ void	init_child_signals(void)
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
 }
+
 void	child_sigquit_handler(int sig)
 {
 	(void)sig;
 	write(2, "Quit (core dumped)\n", 19);
 }
 
-void    handle_heredoc_sigint(int sig_num)
+void	handle_heredoc_sigint(int sig_num)
 {
-    (void)sig_num;
-    g_last_signal = 130;
-    write(1, "\n", 1);
-    close(STDIN_FILENO);
+	(void)sig_num;
+	g_last_signal = 130;
+	write(1, "\n", 1);
+	close(STDIN_FILENO);
 }
 
 void	handle_sigint(int sig_num)
