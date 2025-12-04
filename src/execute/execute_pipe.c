a68ba4632d40a017dcad7aa8442b1370eb41a00d
Thiago Kenji Uemura <tkenji-u@c3r4p2.42sp.org.br>
2025-12-04 18:41:40 -0300

    Norm errors fixed

--- src/execute/execute_pipe.c
+++ src/execute/execute_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 13:59:08 by liferrei          #+#    #+#             */
-/*   Updated: 2025/12/04 18:27:22 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2025/12/04 18:40:54 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,37 +42,41 @@ static pid_t	spawn_child(t_shell *data, t_cmd *cmd, int *fd_pipe)
 	return (pid);
 }
 
-static pid_t	exec_command(t_shell *data, t_cmd *cmd, int *fd_pipe, int next, int is_pipe)
+static pid_t	exec_command(t_shell *data, t_cmd *cmd,
+			int *fd_pipe, int is_pipe)
 {
-	if (cmd->is_builtin && !next && !is_pipe)
+	if (cmd->is_builtin && !cmd->next && !is_pipe)
 		return (handle_single_builtin(data, cmd));
 	return (spawn_child(data, cmd, fd_pipe));
 }
 
+static pid_t	handle_pipe_step(t_shell *data, t_cmd *cmd,
+			int *fd_in, int is_pipe)
+{
+	int		fd_pipe[2];
+	pid_t	pid;
+
+	setup_cmd_fds(cmd, fd_in, fd_pipe);
+	pid = exec_command(data, cmd, fd_pipe, is_pipe);
+	cleanup_fds(cmd, fd_in, fd_pipe);
+	return (pid);
+}
+
 int	execute_pipeline(t_shell *data, t_cmd *cmd_list)
 {
-	t_cmd	*cmd;
 	int		fd_in;
-	int		fd_pipe[2];
 	pid_t	last_pid;
-	int		has_next;
 	int		is_pipe;
 
-	cmd = cmd_list;
 	fd_in = STDIN_FILENO;
 	last_pid = -1;
 	is_pipe = 0;
 	if (cmd_list && cmd_list->next)
 		is_pipe = 1;
-
-	while (cmd)
+	while (cmd_list)
 	{
-		setup_cmd_fds(cmd, &fd_in, fd_pipe);
-		has_next = (cmd->next != NULL);
-		// ALTERAÇÃO 3: Passar 'is_pipe' para a função
-		last_pid = exec_command(data, cmd, fd_pipe, has_next, is_pipe);
-		cleanup_fds(cmd, &fd_in, fd_pipe);
-		cmd = cmd->next;
+		last_pid = handle_pipe_step(data, cmd_list, &fd_in, is_pipe);
+		cmd_list = cmd_list->next;
 	}
 	wait_for_children(data, last_pid);
 	return (data->last_exit_status);
